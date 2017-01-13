87566361c1a864bcc715b6d72973cfae7aff3873
felipejosebento <felipejosebento@outlook.com>
2017-01-13 20:01:48 -0200

    Adicao de comentarios
    Aloha!

--- Aspirador.R
+++ Aspirador.R
@@ -1,6 +1,18 @@
 source("Estado.R")
 
-## Classe e métodos para o problema do Aspirador de pó 2x2
+## Classe de métodos para o problema do Aspirador de pó 2x2#
+## Temos que o aspirador pode realizar quatro procedimentos:
+## I) aspirar sujeira do quadrado em que se encontra
+## II) mover para o quadrado da direita 
+## III) moverpara o quadrado de baixo 
+## IV) mover para o quadrado da esquerda
+## V) mover para o quadrado de cima
+## Sendo que sempre é preciso verificar a possibilidade do movimento a ser executado. 
+## Também temos o seguinte custo 
+## a) 1 para andar para direita ou esquerda
+## b) 2 para aspirar a sujeira
+## c) 3 para andar para cima ou para baixo
+
 Aspirador <- function(desc = NULL, pai = NULL){
   
   e <- environment()
@@ -24,6 +36,8 @@ Ops.Aspirador = function(obj1,obj2){
 }
 
 ## Sobrecarga da função genérica "print" do R
+## (A Q1 Q2 Q3 Q4), temos como A a posição atual do quadrado(1 a 4) e os Qs 
+## a informaçoes se eles estão sujos(1) ou limpos(0)
 print.Aspirador <- function(obj) {
   cat(" __ __     ___ ___\n|Q1|Q2| : |",obj$desc[2],"|",obj$desc[3],"|\n|Q3|Q4|   |",obj$desc[4],"|",obj$desc[5],"|\n")
   cat("Aspirador: Q", obj$desc[1], "\n")
@@ -38,6 +52,7 @@ heuristica.Aspirador <- function(atual){
   if(is.null(atual$desc))
     return(Inf)
   ## h(obj) = Q1 + Q2 + Q3 + Q4
+  ## No caso a equação acima se baseia na soma dos estados "sujos" do estado atual
   return(sum(atual$desc[2:5]))
 }
 
@@ -54,7 +69,7 @@ geraFilhos.Aspirador <- function(obj) {
   sAtual <- as.numeric(desc[desc[1]+1]) ##sujo atualmente??? desc[1] da quadrado atual
   
   ## gera filhos usando todos os operadores 
-    ##caso pode limpar
+  ##caso pode limpar
   if(sAtual == 1){
     
     novo <-desc
